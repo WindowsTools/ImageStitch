a236bcedbda4dd0d81a5dcdbb3d012cc9ee319ea
ZTI <zhaotianff@163.com>
2019-12-04 17:33:03 +0800

    First Commit

---
+++ ImageStitch/ImageStitch/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageStitch
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : TianXiaTech.BlurWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "图像文件|*.jpg;*.bmp;*.png";
+            openFileDialog.Multiselect = true;
+
+            if(openFileDialog.ShowDialog() == true)
+            {
+                StitchWindow stitchWindow = new StitchWindow(openFileDialog.FileNames);
+                stitchWindow.ShowDialog();
+            }
+        }
+    }
+}
---
+++ ImageStitch/ImageStitch/StitchWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImageStitch
+{
+    /// <summary>
+    /// StitchWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class StitchWindow : Window
+    {
+        public StitchWindow(params string[] files)
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var delta = e.Delta / 100f;
+
+            this.scale.ScaleX += delta;
+            this.scale.ScaleY += delta;
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+                return;
+
+            var grid = sender as Grid;
+            var pos = e.GetPosition(grid);
+
+            ShowStatus($"X= {pos.X},Y = {pos.Y}");
+
+            //Canvas.SetLeft(grid, pos.X);
+            //Canvas.SetTop(grid, pos.Y);
+        }
+
+        private void ShowStatus(string str)
+        {
+            this.status.Content = str;
+        }
+    }
+}
